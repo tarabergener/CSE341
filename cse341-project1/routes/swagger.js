fd67e85ca128e1ce8df35d9dfd17ddabf7ad9e07
Tara Bergener <tara.kelly211@gmail.com>
2024-11-21 08:20:39 -0500

    added to new folder

--- cse341-project1/routes/swagger.js
+++ cse341-project1/routes/swagger.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
+const swaggerDocument = require('../../swagger.json');
 router.use('/api-docs', swaggerUI.serve);
 router.get('/api-docs', swaggerUI.setup(swaggerDocument));
 
